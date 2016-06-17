8201642cfc77b03438864a1dd68753c295e59988
Pavisinha <sinharoypavitra@yahoo.com>
2016-06-17 13:57:47 +0530

    Login page

---
+++ app/src/main/java/com/sinha/pavi/swachh/Maind.java
@@ -0,0 +1,13 @@
+package com.sinha.pavi.swachh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Maind extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maind);
+    }
+}
